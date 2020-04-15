226bfd562fb5d02b21833aedcb5a777463806473
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-14 22:28:38 -0400

    server is not fixed

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
