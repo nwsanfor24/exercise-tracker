73248857c19ce7caf095b17b58bdb31b894b5ab5
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-14 22:30:05 -0400

    changed single quotes to double

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -14,7 +14,7 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
 });
 
