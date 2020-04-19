f6e56176946a9dec4f96c6ae8bab7c1e2ccca088
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-19 15:12:24 -0400

    made a create-exercise form and installed react-datepicker

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercises extends Component {
     constructor(props) {
@@ -86,6 +88,37 @@ export default class CreateExercises extends Component {
                                 })
                             }
                         </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration (in minutes): </label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            value={this.state.duration}
+                            onChange={this.onChangeDuration}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
                     </div>
                 </form>
             </div>
